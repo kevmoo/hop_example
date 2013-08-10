59322c6716e10d8310874fbcc27aac46071595bc
Kevin Moore <kevin@thinkpixellab.com>
2013-08-10 14:17:07 -0500

    testing with hop

--- test/harness_console.dart
+++ test/harness_console.dart
@@ -1,9 +1,16 @@
 library test.example;
 
 import 'package:unittest/unittest.dart';
+import 'package:unittest/vm_config.dart';
 import 'package:example_hop/example.dart';
 
 void main() {
+  testCore(new VMConfiguration());
+}
+
+void testCore(Configuration testCfg) {
+  unittestConfiguration = testCfg;
+
   test('sum', () {
 
     expect(sum(1,2), 3);
